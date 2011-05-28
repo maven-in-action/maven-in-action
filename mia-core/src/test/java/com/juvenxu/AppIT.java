0ddb333749a583dca27efad5035923f5025a9883
Juven Xu <juven.xu@outsofting.com>
2011-05-28 20:51:57 +0800

    run ITs within a profile

---
+++ mia-core/src/test/java/com/juvenxu/AppIT.java
@@ -0,0 +1,13 @@
+package com.juvenxu;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppIT
+{
+    @Test
+    public void testApp()
+    {
+        Assert.assertTrue( true );
+    }
+}
